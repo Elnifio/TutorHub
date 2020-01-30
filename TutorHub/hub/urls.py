bed6b3817c462a6c92112258948e69d05d2b294c
Elnifio <elnifio0519@gmail.com>
2020-01-29 22:18:26 -0500

    Update a JQuery
    Update Login page's JQuery

--- TutorHub/hub/urls.py
+++ TutorHub/hub/urls.py
@@ -4,4 +4,5 @@ import hub.views
 
 urlpatterns = [
     path("login", hub.views.login_page),
+    path("auth", hub.views.validate_user),
 ]
--- TutorHub/hub/views.py
+++ TutorHub/hub/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from hub.models import User, Event
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def login_page(request):
     return render(request, 'hub/login.html', {})
+
+def validate_user(request):
+    request.encoding="utf-8"
+    status = {"user": "", "status": False}
+    if 'name' in request.GET and request.GET['name']:
+        user_name = request.GET['name']
+    else:
+        return JsonResponse(status)
+    # TODO
+    return JsonResponse(status)
