c2de94f6d63b4f00c65f5e1bc8ecd907958b412d
Elnifio <elnifio0519@gmail.com>
2020-01-30 22:54:35 -0500

    Added Register page
    Added Sign up page

--- TutorHub/hub/urls.py
+++ TutorHub/hub/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', hub.views.homepage, name="homepage"),
     path("login", hub.views.login_page, name="login"),
     path("auth", hub.views.validate_user, name="auth"),
+    path("register", hub.views.register_page, name="register"),
+    path("register_page_2", hub.views.register_user, name="register_step_2"),
 ]
--- TutorHub/hub/views.py
+++ TutorHub/hub/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from hub.models import User, Event
 from django.http import HttpResponse, JsonResponse
 
+unsafe_password = [
+    '123456',
+    'abcdef',
+    'password'
+]
+
 # Create your views here.
 def homepage(request):
     if not 'login' in request.COOKIES:
@@ -13,6 +19,58 @@ def homepage(request):
 
 def login_page(request):
     return render(request, 'hub/login.html', {})
+
+
+def register_page(request):
+    return render(request, 'hub/register.html', {
+        'no_name': False,
+        "empty_password": False,
+        "invalid_password": False,
+        'not_match': False,
+        'no_email': False
+    })
+
+
+def register_user(request):
+    user_name = ""
+    user_email = ""
+    password = ""
+
+    no_name = False
+    empty_password = False
+    invalid_password = False
+    not_match = False
+    no_email = False
+
+    if not ('name' in request.GET and request.GET['name']):
+        no_name = True
+    if not ('pwd' in request.GET and request.GET['pwd']):
+        empty_password = True
+    elif request.GET['pwd'] in unsafe_password:
+        invalid_password = True
+    
+    if not ("confirm" in request.GET and request.GET['confirm']):
+        not_match = True
+    elif not request.GET['pwd'] == request.GET['confirm']:
+        not_match = True
+    
+    if not ('email' in request.GET and request.GET['email']):
+        no_email = True
+
+    if not (not no_name and not empty_password and not invalid_password and not not_match):
+        return render(request, 'hub/register.html', {
+            'no_name': no_name,
+            'empty_password': empty_password,
+            'invalid_password': invalid_password,
+            'not_match': not_match,
+            'no_email': no_email
+        })
+
+    user_name = request.GET['name']
+    password = request.GET['pwd']
+    user_email = request.GET['email']
+    
+
 
 def validate_user(request):
     # print(request.COOKIES)
