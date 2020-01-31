ec18bc9bbb8023ad2c1891b8f1a1abe23f134949
Elnifio <elnifio0519@gmail.com>
2020-01-30 23:44:58 -0500

    Edited the user model
    Edited the user model

--- TutorHub/hub/models.py
+++ TutorHub/hub/models.py
@@ -3,11 +3,12 @@ import json
 
 # This is the user model that contains all users that can log in to our website.
 class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
     username = models.TextField();
     password = models.TextField();
     email = models.EmailField();
-    is_admin = models.IntegerField(default=0);
-    is_publisher = models.IntegerField(default=0);
+    is_admin = models.BooleanField(default=False);
+    is_publisher = models.BooleanField(default=False);
     preference = models.TextField(default="{}");
     tags = models.TextField(default="{}");
     
